4bfdba3e62e0c130dd95ce146c2af4c880fb07d5
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-15 01:47:39 -0500

    deployed as UUPS (universally upgradeable proxy standard)

--- test/Mars.ts
+++ test/Mars.ts
@@ -3,11 +3,18 @@ import assert from 'assert';
 
 before('get factories', async function(){
     this.Mars = await hre.ethers.getContractFactory('Mars');
+    this.MarsV2 = await hre.ethers.getContractFactory('MarsV2');
+
 })
 
 it('goes to mars', async function(){
-    // const mars = await this.Mars.deploy();
-    const mars = await hre.upgrades.deployProxy(this.Mars);
+    //Deploys 3 contracts: proxy, proxyadmin, mars contract
+    // const mars = await hre.upgrades.deployProxy(this.Mars);
+    const mars = await hre.upgrades.deployProxy(this.Mars, {kind: 'uups'});
 
     assert(await mars.name() === 'Mars');
+
+    const marsV2 = await hre.upgrades.upgradeProxy(mars, this.MarsV2);
+
+    assert (await marsV2.version() === "v2");
 })
